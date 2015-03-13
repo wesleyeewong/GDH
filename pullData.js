94f8125437b6d7a502eecc14d1b0dd39ade590f1
wesleyeewong <wesleywong1993@gmail.com>
2015-03-12 20:00:33 -0400

    stuff

--- pullData.js
+++ pullData.js
@@ -16,6 +16,12 @@ function init() {
 	loadJSON(function(response) {
 		var actual_JSON=JSON.parse(response);
 		console.log(actual_JSON);
+		console.log(actual_JSON.data.children[0].data.title);
+		var stuff = "";
+		for (var i = 0; i < 5; i++) {
+			stuff += "<div class='rows'> <img src='" + actual_JSON.data.children[i].data.thumbnail + "' width=10px height=10px>" + "<a href='" + actual_JSON.data.children[i].data.url + "'>" + actual_JSON.data.children[i].data.title + "</a></div>";
+			document.getElementById('content').innerHTML=stuff;
+		}
 	});
 }
 
